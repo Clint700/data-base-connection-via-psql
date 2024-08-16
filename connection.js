d6b058a9d1f73586b22900d9fc97fac7681ab617
CLINTON <clintonchiemeka@gmail.com>
2024-08-16 17:00:29 +0100

    push the bookshop into a new file in my local directory.

--- connection.js
+++ connection.js
@@ -1,4 +1,5 @@
-const { Client } = require('pg');
+const { Client } = require("pg");
+const fs = require("fs");
 
 const client = new Client();
 
@@ -9,10 +10,17 @@ client
     return client.query("SELECT * FROM bookshop");
   })
   .then((result) => {
-    console.log(result.rows);
+    const data = result.rows;
+    data.map((info) => {
+      info.book_id = info.unique_identifier;
+      delete info.unique_identifier;
+    });
+    const dataFile = JSON.stringify({ Bookshop: data }, null, 4);
+    fs.writeFile("bookshop.json", dataFile, () => {
+        console.log('success')
+    });
     client.end();
   })
   .catch((err) => {
     console.log("connection error:", err);
-  })
-  
+  });
